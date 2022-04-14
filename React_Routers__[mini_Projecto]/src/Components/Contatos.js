a911817907bbabccfba2116c0157dc680f782e90
Fábio Junik <fabiojunik@gmail.com>
2022-04-15 00:43:01 +0100

    Pagina de contato criada

--- React_Routers__[mini_Projecto]/src/Components/Contatos.js
+++ React_Routers__[mini_Projecto]/src/Components/Contatos.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import './Contatos.module.css';
+import styles from './Contatos.module.css';
+import foto from '../img/contato.jpg'
+import Head from './Head';
 
 const Contatos = () => {
   return (
-    <div>
-        <h1>Concatos</h1>
-    </div>
+    <section className={`${styles.contatos} animeLeft`}>
+      <Head title='Ranek | Contato' description='Entre em contato'/>
+      <img src={foto} alt='maquina de escrever'/>
+      <div >
+        <h1>Entre em Contacto.</h1>
+        <ul className={styles.dados}>
+          <li>fabiojunik@gmail.com</li>
+          <li>994 305 673</li>
+          <li>Rua inorade II, Morro Bento</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
